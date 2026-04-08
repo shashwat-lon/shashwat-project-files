5252eb45eaf1308cb0b32b1b1b23979d592efb21
shashwat-lon <tareshashwat526@gmail.com>
2026-04-08 12:32:48 +0530

    Implement user authentication routes and views

---
+++ controllers/user.js
@@ -0,0 +1,60 @@
+const passport=require("passport");
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/WrapAsync.js");
+const {saveRedirectUrl}=require("../middleware.js");
+module.exports.renderSignup=(req,res)=>
+    {
+        res.render("users/user.ejs");
+    };
+
+module.exports.signup=async(req,res)=>
+        {
+            try{
+            let {username,email,password}=req.body;
+            const newUser=new User({email,username});
+            const registeredUser=await User.register(newUser,password);
+            console.log(registeredUser);
+            req.login(registeredUser,(err)=>
+                {
+                    if(err)
+                    {
+                        return next(err);
+                    }
+                    req.flash("success","Welcome to ELITE-ESCAPES");
+                    return res.redirect("/listings");
+                });
+          
+            
+            }
+            catch(e)
+            {
+                req.flash("error",e.message);
+                res.redirect("/signup");
+            }
+        };
+
+module.exports.renderLogin=(req,res)=>
+    {
+        res.render("users/login.ejs");
+    };
+
+module.exports.login=async(req,res)=>
+    {
+        req.flash("success","Welcome Back to Elite-Escapes!!");
+        let redirectUrl=res.locals.redirectUrl || "/listings";
+        res.redirect(redirectUrl);
+    
+    };
+
+module.exports.logout=(req,res,next)=>
+    {
+        req.logout((err)=>
+        {
+            if(err)
+            {
+                return next(err);
+            }
+            req.flash("success","You are Logged Out!");
+            res.redirect("/listings");
+        });
+    };
